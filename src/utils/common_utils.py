c8cea190b46bfd5d41089ee3d6ffca0e57ecb751
Sebastian Hidalgo <105991954+seb-hdz@users.noreply.github.com>
2023-04-22 16:26:29 -0500

    📝 docs (common_utils.py): document 'multi_compare' function

--- src/utils/common_utils.py
+++ src/utils/common_utils.py
@@ -1,4 +1,16 @@
-def multi_compare(value: float, conditions: list[float]):
+def multi_compare(value: float, conditions: list[float]) -> int or None:
+    """
+    Returns the index of the first element in the `conditions` list that the `value` is less than.
+    
+    Args:
+    - value: A float representing the value to be compared against the `conditions`.
+    - conditions: A list of floats representing the threshold values to be compared against the `value`.
+    
+    Returns:
+    - If the `value` is less than an element in the `conditions` list, returns the index of that element.
+    - If the `value` is greater than or equal to all elements in the `conditions` list, returns None.
+    """
     for index, condition in enumerate(conditions):
         if value < condition:
             return index
+    return None
